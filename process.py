490f49334add2472d36d7a89a389c97c097518ce
npcasler <ncasler@email.arizona.edu>
2015-05-13 17:13:58 -0700

    Added projection transformations

--- process.py
+++ process.py
@@ -1,9 +1,38 @@
 ## Load a file
 
-import csv, pyqtree
-from shapely.geometry import Point, Polygon
+try:
+    from osgeo import ogr, osr, gdal
+except:
+    sys.exit('ERROR: cannot find GDAL/OGR modules')
+
+def gdal_error_handler(err_class, err_num, err_msg):
+    errtype = {
+            gdal.CE_None:'None',
+            gdal.CE_Debug:'Debug',
+            gdal.CE_Warning:'Warning',
+            gdal.CE_Failure:'Failure',
+            gdal.CE_Fatal:'Fatal'
+    }
+    err_msg = err_msg.replace('\n',' ')
+    err_class = errtype.get(err_class, 'None')
+    print 'Error Number: %s' % (err_num)
+    print 'Error Type: %s' % (err_class)
+    print 'Error Message: %s' % (err_msg)
 
 
+try:
+    import csv, pyqtree
+    from shapely.geometry import Point, Polygon
+except: 
+    sys.exit('Error: cannot find dependent libraries')
+
+if __name__ == '__main__':
+    # install error handler
+    gdal.PushErrorHandler(gdal_error_handler)
+
+
+
+ogr.UseExceptions()
 
 xMin = -131.35
 yMin = 38
@@ -13,6 +42,16 @@ yMax = 56
 spindex = pyqtree.Index(bbox=[xMin,yMin,xMax,yMax])
 
 overlapBbox = (-129,44,-118,48.31)
+
+wgs = osr.SpatialReference()
+wgs.ImportFromEPSG(4326)
+
+# This is the coordinate system used for the plot measurements
+# This example uses UTM zone 12N, will need to parameterize this
+utm = osr.SpatialReference()
+utm.ImportFromEPSG(32612)
+
+transform = osr.CoordinateTransformation(wgs, utm)
 
 with open('plotnodes.csv', 'rb') as p:
     reader = csv.DictReader(p)
@@ -30,18 +69,50 @@ with open('plotnodes.csv', 'rb') as p:
         plots.append(plotPoly)
     print plots[5]
 
+src  =  open('f119_2012_doy201_1pm_cc.csv', 'rb')
+tmp = open('cc_tmp.csv', 'wb')
+writer = csv.writer(tmp)
+for row in csv.reader(src):
+    # Remove rows where all fields are empty
+    if any(field.strip() for field in row):
+       
+        writer.writerow(row)
+src.close()
+tmp.close()
 
-            
 
-with open('Abies_amabilis.csv', 'rb') as f:
+
+
+pointDataSource = ogr.Open("cc.vrt")
+print pointDataSource.__len__()
+lyr = pointDataSource.GetLayer(0)
+#print lyr
+for feat in lyr:
+    geom = feat.GetGeometryRef()
+    geom.Transform(transform)
+    print feat
+    print geom.ExportToWkt()
+     
+'''
+with open('f119_2012_doy201_1pm_cc.csv', 'rb') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        point = Point(float(row['longitude']), float(row['latitude']))
-        #print point.bounds
-        spindex.insert(item=point, bbox=point.bounds)
+        print row['LONGITUDE']
+        xLon = row['LONGITUDE']
+        xLat = row['LATITUDE']
+        # Check for null coordinates
+        if bool(xLon.strip()) and bool(xLat.strip()):
+
+            point = Point(float(xLon), float(xLat))
+            geom = ogr.CreateGeometryFromWkb(point.wkb)
+            geom.Transform(transform)
+            print geom.GetEnvelope
+            spindex.insert(item=geom, bbox=geom.GetEnvelope())
+        else: 
+            print "Coordinates missing"
     
     matches = spindex.intersect(overlapBbox)
     for i in plots:
         plotMatches = spindex.intersect(i.bounds)
         print `i` + `len(plotMatches)`
-    print len(matches)
+   ''' 
